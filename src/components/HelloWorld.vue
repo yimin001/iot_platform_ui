04e5df9f9eafff69356a24d624e16405831a8bda
yimin001 <540019320@qq.com>
2019-05-09 13:53:18 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}
+    <a href="VueDet.html">测试实例</a>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Vue'
+    }
+  }
+}
+</script>
